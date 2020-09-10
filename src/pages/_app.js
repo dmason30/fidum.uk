5428fe9eea792e85d27d8ae0166eb735ec1a38d9
dmason30 <danmason@spendology.com>
2020-09-10 12:52:43 +0100

    add fathom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,34 @@
-import '@/css/tailwind.css'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+
+import '@/css/tailwind.css'
+
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        // Initialize Fathom when the app loads
+        Fathom.load('ICJIJBJL', {
+            includedDomains: ['fidum.uk']
+        })
+
+        function onRouteChangeComplete() {
+            Fathom.trackPageview()
+        }
+        // Record a pageview when route changes
+        router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+        // Unassign event listener
+        return () => {
+            router.events.off('routeChangeComplete', onRouteChangeComplete)
+        }
+    }, [])
+
   return (
     <div className="antialiased grid grid-cols-1">
       <Head>
