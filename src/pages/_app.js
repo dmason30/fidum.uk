ddad5dfc956adda4002778c2dfdbfbbecb232777
dmason30 <danmason@spendology.com>
2020-09-10 13:34:53 +0100

    Add custom domain

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }) {
     useEffect(() => {
         // Initialize Fathom when the app loads
         Fathom.load('ICJIJBJL', {
+            url: 'https://canidae.fidum.uk/script.js',
             includedDomains: ['fidum.uk']
         })
 
